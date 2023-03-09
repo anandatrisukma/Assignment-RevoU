e255133950ecb26ebfcf9df8185037d56560a204
Ananda Trisukma Kusumah <anandatrisukma2@gmail.com>
2023-03-09 19:15:18 +0700

    revamp image slider

--- JS/index.js
+++ JS/index.js
@@ -18,7 +18,7 @@ carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("testimony");
+  var x = document.getElementsByClassName("testimony-wrapper");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
